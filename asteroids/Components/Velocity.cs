3efea5e4bf866ba5f4299f0850faf566278144f8
Michael Novikov <sigod.mail@gmail.com>
2012-09-15 23:30:46 +0300

    Added `Velocity` component;

---
+++ asteroids/Components/Velocity.cs
@@ -0,0 +1,50 @@
+﻿using Artemis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asteriods.Components {
+	class Velocity : Component {
+		private float velocity;
+		private float angle;
+
+
+		public Velocity() { }
+
+		public Velocity(float velocity, float angle) {
+			this.velocity = velocity;
+			this.angle = angle;
+		}
+
+
+		public float Speed {
+			get {
+				return this.velocity;
+			}
+			set {
+				velocity = value;
+			}
+		}
+
+		public float Angle {
+			get {
+				return this.angle;
+			}
+			set {
+				angle = value;
+			}
+		}
+
+		public void AddAngle(float angle) {
+			this.angle = (this.angle + angle) % 360;
+		}
+
+		public float AngleAsRadians {
+			get {
+				return (float)Math.PI * angle / 180.0f;
+			}
+		}
+	}
+}
