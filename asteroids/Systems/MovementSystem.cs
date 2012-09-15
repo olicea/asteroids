5b594ce42b324d9984142163b276d3b083db2d7e
Michael Novikov <sigod.mail@gmail.com>
2012-09-16 02:30:03 +0300

    Added `MovementSystem` system;

---
+++ asteroids/Systems/MovementSystem.cs
@@ -0,0 +1,31 @@
+﻿using Artemis;
+using asteriods.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asteriods.Systems {
+	class MovementSystem : EntityProcessingSystem {
+		private ComponentMapper<Transform> transformMapper;
+		private ComponentMapper<Velocity> velocityMapper;
+
+
+		public MovementSystem() : base(typeof(Transform), typeof(Velocity)) { }
+
+
+		public override void Initialize() {
+			this.transformMapper = new ComponentMapper<Transform>();
+			this.velocityMapper = new ComponentMapper<Velocity>();
+		}
+
+		public override void Process(Entity entity) {
+			Transform transform = transformMapper.Get(entity);
+			Velocity velocity = velocityMapper.Get(entity);
+
+			transform.X += (TrigLUT.Cos(velocity.AngleAsRadians) * velocity.Speed * world.Delta);
+			transform.Y += (TrigLUT.Sin(velocity.AngleAsRadians) * velocity.Speed * world.Delta);
+		}
+	}
+}
