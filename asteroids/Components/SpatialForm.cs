d341466fd4fa33c8272907d29d6c1848f6302001
Michael Novikov <sigod.mail@gmail.com>
2012-09-16 00:15:32 +0300

    Added `SpatialForm` component;

---
+++ asteroids/Components/SpatialForm.cs
@@ -0,0 +1,30 @@
+﻿using Artemis;
+using asteriods.Spatials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asteriods.Components {
+	class SpatialForm : Component {
+		private SpatialForms form;
+
+
+		public SpatialForm() { }
+
+		public SpatialForm(SpatialForms form) {
+			this.form = form;
+		}
+
+
+		public SpatialForms Form {
+			get {
+				return this.form;
+			}
+			set {
+				this.form = value;
+			}
+		}
+	}
+}
