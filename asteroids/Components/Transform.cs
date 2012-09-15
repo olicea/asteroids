b779abfc4bad4df61c2ee5f6f675a9ff8334fc7c
Michael Novikov <sigod.mail@gmail.com>
2012-09-15 23:26:43 +0300

    Added `Transform` component;

---
+++ asteroids/Components/Transform.cs
@@ -0,0 +1,76 @@
+﻿using Artemis;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asteriods.Components {
+	class Transform : Component {
+		private Vector3 coordinates;
+
+
+		public Transform() { }
+
+		public Transform(Vector3 coordinates) {
+			this.coordinates = coordinates;
+		}
+
+
+		public Vector3 Coordinates {
+			get {
+				return this.coordinates;
+			}
+			set {
+				this.coordinates = value;
+			}
+		}
+
+		public float X {
+			get {
+				return this.coordinates.X;
+			}
+			set {
+				this.coordinates.X = value;
+			}
+		}
+
+		public float Y {
+			get {
+				return this.coordinates.Y;
+			}
+			set {
+				this.coordinates.Y = value;
+			}
+		}
+
+		public void SetLocation(float x, float y) {
+			this.coordinates.X = x;
+			this.coordinates.Y = y;
+		}
+
+		public float Rotation {
+			get {
+				return this.coordinates.Z;
+			}
+			set {
+				this.coordinates.Z = value;
+			}
+		}
+
+		public void AddRotation(float angle) {
+			this.coordinates.Z = (this.coordinates.Z + angle) % 360;
+		}
+
+		public float RotationAsRadians {
+			get {
+				return (float)Math.PI * this.coordinates.Z / 180.0f;
+			}
+		}
+
+		public float DistanceTo(Transform transform) {
+			return Artemis.Utils.Distance(transform.X, transform.Y, X, Y);
+		}
+	}
+}
