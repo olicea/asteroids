d57cb207c3422764c43237a51d2e0a693ddbd1dd
Michael Novikov <sigod.mail@gmail.com>
2012-09-16 02:43:34 +0300

    Added `RenderSystem` system;

---
+++ asteroids/Systems/RenderSystem.cs
@@ -0,0 +1,49 @@
+﻿using Artemis;
+using asteriods.Components;
+using asteriods.Spatials;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asteriods.Systems {
+	class RenderSystem : EntityProcessingSystem {
+		private GraphicsDevice graphicsDevice;
+		private SpriteBatch spriteBatch;
+
+		private ComponentMapper<Transform> transformMapper;
+		private ComponentMapper<SpatialForm> spatialFormMapper;
+
+
+		public RenderSystem(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+			: base(typeof(Transform), typeof(SpatialForm)) {
+				this.graphicsDevice = graphicsDevice;
+				this.spriteBatch = spriteBatch;
+		}
+
+
+		public override void Initialize() {
+			this.transformMapper = new ComponentMapper<Transform>(world);
+			this.spatialFormMapper = new ComponentMapper<SpatialForm>(world);
+		}
+
+		public override void Process(Entity entity) {
+			Transform transform = this.transformMapper.Get(entity);
+
+			if (transform.X < 0
+				&& transform.X >= this.graphicsDevice.Viewport.Width
+				&& transform.Y < 0
+				&& transform.Y >= this.graphicsDevice.Viewport.Height) {
+					return;
+			}
+
+			switch (this.spatialFormMapper.Get(entity).Form) {
+				case SpatialForms.Player:
+					PlayerForm.Render(this.spriteBatch, transform);
+					break;
+			}
+		}
+	}
+}
